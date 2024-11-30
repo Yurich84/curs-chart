e278a4e235717a2ee5a4de99314755c365464267
Yurich <yuriy.timchuk@cheitgroup.com>
2024-11-30 23:04:13 +0000

    feat: add styles, store in session

--- src/components/ChartWrapper.vue
+++ src/components/ChartWrapper.vue
@@ -1,27 +1,27 @@
 <template>
     <h2>{{ currentBank }} ({{ currentCurrency }})</h2>
+    <div class="chart-links">
+        <span @click="goToData(BANK_PRIVAT, 'USD')">Privat USD</span>
+        <span @click="goToData(BANK_PRIVAT, 'EUR')">Privat EUR</span>
+        <span @click="goToData(BANK_MONO, 'EUR')">Mono EUR</span>
+    </div>
     <Chart
         v-if="!loading"
         :key="chartComponentKey"
         :data="currentData"
     />
-    <div class="chart-links">
-        <a href="#" @click="goToData(BANK_PRIVAT, 'USD')">Privat USD</a>
-        <a href="#" @click="goToData(BANK_PRIVAT, 'EUR')">Privat EUR</a>
-        <a href="#" @click="goToData(BANK_MONO, 'USD')">Mono USD</a>
-    </div>
 </template>
 
 <script setup>
-import Chart from '@/App.vue'
+import Chart from '@/components/Chart.vue'
 import { ref } from 'vue'
 import { BANK_MONO, BANK_PRIVAT } from '@/constants/constants.js'
 import { getCursData } from '@/services/api.js'
 
 const chartComponentKey = ref(0)
 
-const currentBank = ref('')
-const currentCurrency = ref('')
+const currentBank = ref(BANK_PRIVAT)
+const currentCurrency = ref('USD')
 const currentData = ref([])
 
 const goToData = (bank, currency) => {
@@ -34,14 +34,35 @@ const goToData = (bank, currency) => {
 
 const loading = ref(true)
 
+fetchData()
+
 async function fetchData () {
-    loading .value= this
-    const { data } = getCursData()
-    currentData.value = data.sort((a, b) => a.timestamp - b.timestamp)
+    loading.value= true
+    const cacheVariable = `${currentBank.value}-${currentCurrency.value}`
+    if (sessionStorage.getItem(cacheVariable)) {
+        currentData.value = JSON.parse(sessionStorage.getItem(cacheVariable))
+    } else {
+        const { data } = await getCursData(currentBank.value, currentCurrency.value)
+        currentData.value = data.sort((a, b) => a.timestamp - b.timestamp)
+        sessionStorage.setItem(cacheVariable, JSON.stringify(currentData.value))
+    }
+
     loading.value = false
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.chart-links {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    margin-bottom: 20px;
 
+    span {
+        color: var(--color-primary);
+        cursor: pointer;
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+}
 </style>
